a4169201e7555810365dbb1d305dc7d3ebe3d4e2
FK <fk@bitsandlikes.de>
2023-07-10 22:35:07 +0200

    Fix newline

--- src/Resources/app/administration/src/app/component/filter/sw-string-filter/index.js
+++ src/Resources/app/administration/src/app/component/filter/sw-string-filter/index.js
@@ -49,8 +49,7 @@ export default {
             currentComparatorOptions: currentComparatorOptions,
             enableManualMode: !!(this.filter.enableManualMode ?? false),
         };
-    }
-    ,
+    },
 
     watch: {
         'filter.value': {
